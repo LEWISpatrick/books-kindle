cd6122707191a3d599475e77de1ae08239a06ed6
LEWISpatrick <133691910+LEWISpatrick@users.noreply.github.com>
2024-06-29 22:55:48 -0300

    added upload books

--- app/api/upload-book/upload-book.ts
+++ app/api/upload-book/upload-book.ts
@@ -2,29 +2,21 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '@/lib/db'; // Assuming db is properly set up to interact with your database
-import { auth } from '@/auth';
-import { NextResponse } from 'next/server';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const user = await auth()
-
-    if (!user || !user.user.id) {
-      console.error('Authentication failed or user ID is missing.')
-      return new NextResponse('Unauthorized', { status: 401 })
-    }
-    if (req.method === 'POST') {
+  if (req.method === 'POST') {
     const { title, author, description, imageUrl, downloadLink, category, userId } = req.body;
 
     try {
-        const book = await db.book.create({ // Call the create method on db.book
+        const book = await db.book.create({
             data: {
-                title,
-                author,
-                description,
-                imageUrl,
-                downloadLink,
-                category,
-                userId, // Ensure userId is included
+                title: title,
+                author: author,
+                description: description,
+                imageUrl: imageUrl,
+                downloadLink: downloadLink,
+                category: category,
+                userId: userId,
             },
         });
         res.status(201).json(book);
